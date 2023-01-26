84eb74cf0d75428ab8994def08b9ab7088e7a502
Tropicalista <francescopepe@email.it>
2023-01-26 13:41:00 +0100

    fix modal

--- popper.php
+++ popper.php
@@ -3,7 +3,7 @@
  * Plugin Name: Popper
  * Plugin URI:  https://formello.net/
  * Description: Popup builder with exit-intent powered by Gutenberg.
- * Version:     0.4.6
+ * Version:     0.4.7
  * Requires Plugins: formello
  * Author:      Formello
  * Author URI:  https://formello.net
